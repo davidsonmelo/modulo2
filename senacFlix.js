f4d52c28756f7829f074c64d7e608bfc5b64269a
davidsonmelo <davidspedrozamelo>
2022-02-03 19:18:31 -0300

    modolo2

---
+++ senacFlix.js
@@ -0,0 +1,8 @@
+console.log("usuario 1: assistindo filme");
+console.log("usuario 2: assistindo série");
+
+setTimeout(function() {
+    console.log('Segundo');
+    console.log('Primeiro');
+}, 3000);
+ console.log('Terceiro');
