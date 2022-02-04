32c6102ad5a5618fceee8898b3be5ead5162e987
davidsonmelo <davidspedrozamelo>
2022-02-03 21:31:12 -0300

    inserir arquivo

---
+++ dados.js
@@ -0,0 +1,19 @@
+
+biscoito = (1234556948n * 10n);
+
+console.log(biscoito);
+
+const a = 5;
+const b = 10;
+const soma = (a + b);
+console.log(soma);
+
+console.log('Hello' + ' ' + "World")
+console.log('Hello' + "World")
+myMame = "Davidson"
+console.log("Hello" + ' ' + myMame)
+myMame2 = "2"
+console.log(`Hello ${myMame2}`)
+var carro = "gol"
+var preco = 10000
+console.log(carro + " " + preco)
