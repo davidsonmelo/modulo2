d4d494626665aea3bcee9deee847c233fc4372c5
davidsonmelo <davidspedrozamelo>
2022-02-08 19:35:36 -0300

    initial commit

--- dados.js
+++ dados.js
@@ -8,12 +8,20 @@ const b = 10;
 const soma = (a + b);
 console.log(soma);
 
-console.log('Hello' + ' ' + "World")
-console.log('Hello' + "World")
-myMame = "Davidson"
-console.log("Hello" + ' ' + myMame)
-myMame2 = "2"
-console.log(`Hello ${myMame2}`)
-var carro = "gol"
-var preco = 10000
-console.log(carro + " " + preco)
+console.log('Hello' + ' ' + "World");
+console.log('Hello' + "World");
+myMame = "Davidson";
+console.log("Hello" + ' ' + myMame);
+myMame2 = "2";
+console.log(`Hello ${myMame2}`);
+
+const carro = "gol"
+const preco = 55000;
+const parcela = 60;
+const entrada = 15000;
+
+console.log("modelo: " + carro + " valor " + preco);
+
+console.log("valor finaciado: " + (preco-entrada))
+
+console.log("valor da parcela: " + (preco-entrada)/parcela);
