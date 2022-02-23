8857c4e20f7042edd2246f2c4f2bb7eb3a227dc4
davidsonmelo <davidsonpedrozamelo@gmail.com>
2022-02-23 17:37:53 -0300

    Exercício de tabela de preço

--- exercicio.js
+++ exercicio.js
@@ -3,15 +3,15 @@
 // Você deverá usar variavéis, operadores matemáticos, lógicos econdicio.
 // Dica: Use o método .parseFloat() para converter. Deverá pesquisar.
 
-// const cadastro = require('readline-sync')
+const cadastro = require('readline-sync')
 
-// const estudante = cadastro.question("Informe o nome do estudante: ")
-// const primeiraNota = parseFloat(cadastro.question("Insira nota 1: ")) 
-// const segundaNota = parseFloat(cadastro.question("Insira nota 2: "))
-// const terceiraNota = parseFloat(cadastro.question("Insira nota 3: ")) 
-// const quartaNota = parseFloat(cadastro.question("Insira nota 4: ")) 
-// const quintaNota = parseFloat(cadastro.question("Insira nota 5: "))
-// const media = (primeiraNota + segundaNota + terceiraNota + quartaNota + quintaNota)/5
+const estudante = cadastro.question("Informe o nome do estudante: ")
+const primeiraNota = parseFloat(cadastro.question("Insira nota 1: ")) 
+const segundaNota = parseFloat(cadastro.question("Insira nota 2: "))
+const terceiraNota = parseFloat(cadastro.question("Insira nota 3: ")) 
+const quartaNota = parseFloat(cadastro.question("Insira nota 4: ")) 
+const quintaNota = parseFloat(cadastro.question("Insira nota 5: "))
+const media = (primeiraNota + segundaNota + terceiraNota + quartaNota + quintaNota)/5
 
 if (media >= 7) {
     console.log("A média de " + estudante + ": " + media + " foi aprovado, parabéns!!!") 
